5a6fe180d98c0a3c384c1d1acee44f3f0259559f
eramoss <edulramos@outlook.com>
2023-12-27 19:02:11 -0300

    adjust debug option

--- tests/libpng-tests.c
+++ tests/libpng-tests.c
@@ -2,8 +2,12 @@
 #include <string.h>
 #include "libimagec.h"
 
+#define IMAGE_TEST_PATH "../../testImage.png"
+#define PNG_COLOR_TYPE_RGB2 (6)
+#define EXPECTED_DEPTH 8
 
-// Simple assert macro
+
+// Simple ASSERT macro
 #define ASSERT(condition) \
     if (!(condition)) {    \
         printf("Test failed: %s, line %d\n", __FILE__, __LINE__); \
@@ -14,22 +18,39 @@ void optimize_dip_depth_color_type(const png_infos *png, png_byte color_type, pn
 
 png_bytep *get_matrix_pointers_RGB(const png_infos *png, png_uint_32 height);
 
+void debug_rgb_values(png_uint_32 width, png_uint_32 height, png_bytep const *row_pointers);
+
 unsigned read_png_test() {
-    png_infos *png = read_png_file("../../testImage.png");
+    png_infos *png = read_png_file(IMAGE_TEST_PATH);
     png_uint_32 width = png_get_image_width(*png->png_ptr, *png->info_ptr);
     png_uint_32 height = png_get_image_height(*png->png_ptr, *png->info_ptr);
     png_byte color_type = png_get_color_type(*png->png_ptr, *png->info_ptr);
     png_byte bit_depth = png_get_bit_depth(*png->png_ptr, *png->info_ptr);
 
+    ASSERT(width > 0);
+    ASSERT(height > 0);
+    ASSERT(color_type == (PNG_COLOR_TYPE_RGB | PNG_COLOR_TYPE_RGB2));
+    ASSERT(bit_depth == EXPECTED_DEPTH);
+
+
     //  Optional
     optimize_dip_depth_color_type(png, color_type, bit_depth);
-
 
     png_bytep *row_pointers = get_matrix_pointers_RGB(png, height);
 
     cleanup_png(png);
+#ifdef DEBUG
+    debug_rgb_values(width, height, row_pointers);
 
-    // Access RGB values for each pixel
+    for (int y = 0; y < height; y++)
+        free(row_pointers[y]);
+    free(row_pointers);
+#endif
+    return 0;
+}
+
+void debug_rgb_values(png_uint_32 width, png_uint_32 height,
+                      png_bytep const *row_pointers) {// Access RGB values for each pixel
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width * 3; x += 3) {
             png_byte red = row_pointers[y][x];
@@ -40,11 +61,6 @@ unsigned read_png_test() {
             printf("Pixel at (%d, %d) - Red: %d, Green: %d, Blue: %d\n", x / 3, y, red, green, blue);
         }
     }
-    
-    for (int y = 0; y < height; y++)
-        free(row_pointers[y]);
-    free(row_pointers);
-    return 0;
 }
 
 png_bytep *get_matrix_pointers_RGB(const png_infos *png, png_uint_32 height) {
