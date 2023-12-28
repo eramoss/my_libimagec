a6ba36f1374873b1ebad217afc7e7d9f97967a2d
eramoss <edulramos@outlook.com>
2023-12-28 14:26:32 -0300

    update matrix tests

--- tests/matrix_tests.h
+++ tests/matrix_tests.h
@@ -27,19 +27,39 @@ unsigned test_create_matrix() {
 }
 
 unsigned test_matrix_set_and_get() {
-    matrix *m = create_matrix(2, 2);
+    matrix *m = create_matrix(4, 3);
 
     // Set values
     matrix_set_(m, 0, 0, 1);
     matrix_set_(m, 0, 1, 2);
-    matrix_set_(m, 1, 0, 3);
-    matrix_set_(m, 1, 1, 4);
+    matrix_set_(m, 0, 2, 3);
+    matrix_set_(m, 1, 0, 4);
+    matrix_set_(m, 1, 1, 5);
+    matrix_set_(m, 1, 2, 6);
+    matrix_set_(m, 2, 0, 7);
+    matrix_set_(m, 2, 1, 8);
+    matrix_set_(m, 2, 2, 9);
+    matrix_set_(m, 3, 0, 'A');
+    matrix_set_(m, 3, 1, 'B');
+    matrix_set_(m, 3, 2, 'C');
 
+
+
+    // Check values
     // Check values
     ASSERT(matrix_get(m, 0, 0) == 1);
     ASSERT(matrix_get(m, 0, 1) == 2);
-    ASSERT(matrix_get(m, 1, 0) == 3);
-    ASSERT(matrix_get(m, 1, 1) == 4);
+    ASSERT(matrix_get(m, 0, 2) == 3);
+    ASSERT(matrix_get(m, 1, 0) == 4);
+    ASSERT(matrix_get(m, 1, 1) == 5);
+    ASSERT(matrix_get(m, 1, 2) == 6);
+    ASSERT(matrix_get(m, 2, 0) == 7);
+    ASSERT(matrix_get(m, 2, 1) == 8);
+    ASSERT(matrix_get(m, 2, 2) == 9);
+    ASSERT(matrix_get(m, 3, 0) == 'A');
+    ASSERT(matrix_get(m, 3, 1) == 'B');
+    ASSERT(matrix_get(m, 3, 2) == 'C');
+
 
     matrix_free(m);
     return 0;
