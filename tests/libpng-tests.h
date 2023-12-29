0811600f03fe1802cd27ffe6ac9fe541c2cfcc9f
eramoss <edulramos@outlook.com>
2023-12-29 11:35:26 -0300

    organized tests

--- tests/libpng-tests.h
+++ tests/libpng-tests.h
@@ -10,7 +10,6 @@
 
 unsigned read_png_test();
 
-void debug_rgb_values(png_uint_32 width, png_uint_32 height, png_bytep const *row_pointers);
 
 unsigned read_png_test() {
     image *png = read_image(IMAGE_TEST_PATH);
@@ -20,48 +19,9 @@ unsigned read_png_test() {
     ASSERT(png->color_type == (PNG_COLOR_TYPE_RGB | PNG_COLOR_TYPE_RGB2));
     ASSERT(png->bit_depth == EXPECTED_DEPTH);
 
-
-#ifdef DEBUG
-    png_bytep *row_pointers = get_matrix_pointers_RGB(png->png_ptr, png->info_ptr, png->height);
-    debug_rgb_values(png->width, png->height, row_pointers);
-    /*
-     cleanup section
- */
-    cleanup_image(png);
-    for (int y = 0; y < png->height; y++)
-        free(row_pointers[y]);
-    free(row_pointers);
-#endif
-
-
+    default_debug_rgb_(png);
     return 0;
 }
-
-/*
- * ----------------------------------------------
- *
- *
- *         UTILS SECTION FOR TESTING
- *
- *
- * ---------------------------------------------
- */
-
-
-void debug_rgb_values(png_uint_32 width, png_uint_32 height,
-                      png_bytep const *row_pointers) {// Access RGB values for each pixel
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width * 3; x += 3) {
-            png_byte red = row_pointers[y][x];
-            png_byte green = row_pointers[y][x + 1];
-            png_byte blue = row_pointers[y][x + 2];
-
-            // For debugging, you can print the RGB values
-            printf("Pixel at (%d, %d) - Red: %d, Green: %d, Blue: %d\n", x / 3, y, red, green, blue);
-        }
-    }
-}
-
 
 #define ALL_LIB_PNG_TESTS \
     read_png_test
--- tests/test_utils.h
+++ tests/test_utils.h
@@ -4,10 +4,45 @@
 
 #ifndef LIBIMAGEC_TEST_UTILS_H
 #define LIBIMAGEC_TEST_UTILS_H
+
+#include "libimagec.h"
 // Simple ASSERT macro
 #define ASSERT(condition) \
     if (!(condition)) {    \
         printf("Test failed: %s, line %d\n", __FILE__, __LINE__); \
         return 1; \
     }
+
+
+void debug_rgb_values(png_uint_32 width, png_uint_32 height,
+                      png_bytep const *row_pointers) {// Access RGB values for each pixel
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width * 3; x += 3) {
+            png_byte red = row_pointers[y][x];
+            png_byte green = row_pointers[y][x + 1];
+            png_byte blue = row_pointers[y][x + 2];
+
+            // For debugging, you can print the RGB values
+            printf("Pixel at (%d, %d) - Red: %d, Green: %d, Blue: %d\n", x / 3, y, red, green, blue);
+        }
+    }
+}
+
+void default_debug_rgb_(image *png) {
+#ifdef DEBUG
+    png_bytep *row_pointers = get_matrix_pointers_RGB(png->png_ptr, png->info_ptr, png->height);
+    debug_rgb_values(png->width, png->height, row_pointers);
+    /*
+     cleanup section
+ */
+    cleanup_image(png);
+    for (int y = 0; y < png->height; y++)
+        free(row_pointers[y]);
+    free(row_pointers);
+#endif
+
+}
+
+
 #endif //LIBIMAGEC_TEST_UTILS_H
+
