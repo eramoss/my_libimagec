3c5afb59d244e3b01ae3b543ed0c8502256fcb8c
eramoss <edulramos@outlook.com>
2023-12-29 10:49:06 -0300

    rename structures

--- include/libimagec.h
+++ include/libimagec.h
@@ -8,17 +8,22 @@
 #include <zlib.h>
 #include "../lib/libpng-1.6.40/png.h"
 
+typedef unsigned char image_byte;
+typedef unsigned short image_uint_16;
+typedef image_uint_16 image_type;
+
 typedef struct {
     png_structp *png_ptr;
     png_infop *info_ptr;
-    png_uint_16 width;
-    png_uint_16 height;
-    png_byte bit_depth;
-    png_byte color_type;
-    png_byte channels;
-    png_byte filter_method;
-    png_byte compression_type;
-    png_byte interlace_type;
+    image_uint_16 width;
+    image_uint_16 height;
+    image_byte bit_depth;
+    image_byte color_type;
+    image_byte channels;
+    image_byte filter_method;
+    image_byte compression_type;
+    image_byte interlace_type;
+    image_type image_type;
     FILE *fp;
 } image;
 
